9fbb203ebef427f27dc749ed1ad4288fa98c96bb
Afaed <44733498+Afaed@users.noreply.github.com>
2021-11-19 15:05:02 -0800

    Removed the my function at the top and rearranged the order of the code.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,18 +1,18 @@
 // Assignment code here
-myfunction = 12
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("#generate");
 
 // Write password to the #password input
 function writePassword() {
  var password=generatePassword();
- var passwordText=document.querySelector("#password")
- passwordText.value = password
- var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
- var passwordLength = 12;
- var password = "";
-for (var i = 0; i <= passwordLength; i++) {
+ var passwordText=document.querySelector("#password");
+ generatePassword({passwordText.value = password{
+     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     var passwordLength = 8;
+     var password = ""});
+
+ for (var i = 0; i <= passwordLength; i++) {
 var randomNumber = Math.floor(Math.random() * chars.length);
 password += chars.substring(randomNumber, randomNumber +1);
 }
