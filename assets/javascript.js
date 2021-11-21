1bd78f201009456f57d56886d18ab9ce8e064fb9
Afaed <44733498+Afaed@users.noreply.github.com>
2021-11-20 23:54:56 -0800

    Created test prompts to experiment with prompts and data saving.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,10 @@
 // Assignment code here
-
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p","q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "@", "#","$", "%","^","&", "*", "(", ")", "[","]",];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var passwordLength = 8;
+var password = "";
 // Get references to the #generate element
 var generateBtn = document.getElementById("#generate");
 
@@ -7,16 +12,21 @@ var generateBtn = document.getElementById("#generate");
 function writePassword() {
  var password=generatePassword();
  var passwordText=document.querySelector("#password");
- generatePassword({passwordText.value = password{
-     var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-     var passwordLength = 8;
-     var password = ""});
 
- for (var i = 0; i <= passwordLength; i++) {
-var randomNumber = Math.floor(Math.random() * chars.length);
-password += chars.substring(randomNumber, randomNumber +1);
-}
-     document.querySelector("password").value = password;
+ window.prompt("what lowercase letter do you want") 
+    if (response = lowercase || response === "ok");
+        else { window.alert("unacceptable");
+    return lowresponse;}
+    console.log("lowerresponse")
+
+
+window.prompt("Which capital letter do you want")
+    if (window.prompt = capitalcase || Response === "ok")
+        ;else {window.prompt("Please pick a acceptable letter");
+        return capitalcase
+    }
+
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+localStorage.getItem("password")
